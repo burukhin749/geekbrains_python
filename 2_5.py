7c1689399668c06577f544bd16fc562c31f5d05e
Dmitrii Burukhin <burukhin749@gmail.com>
2021-12-24 02:10:38 +0300

    Update 2_5

--- 2_5.py
+++ 2_5.py
@@ -1,7 +1,13 @@
-prices = [41.5, 56, 150.33, 19.8, 241.99, 2.3, 43.3, 56.44, 45.7, 34, 9.72, 98]
+# Список с ценами
+prices = [41.5, 56, 150.33, 19.8,
+          241.99, 2.3, 43.3, 56.44,
+          45.7, 34, 9.72, 98]
 
-print(f"\n\n{'*' * 35} А_1\n")
+# Сортировка по возрастанию (одинаковые ID)
+print(id(prices))
+prices.sort()
+print(id(prices))
 
 for i in prices:
-    rub, kop = str(f"{i:.2f}").split(".")
-    print(f"{rub} руб {kop} коп,", end=" ")
+    r, kk = str(f"{i:.2f}").split('.')
+    print(f"{r} руб {kk} коп,", end=" ")
